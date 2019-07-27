6f154446ddf86b3991abefcc29c16a508eeee388
idfxken <idefix6@gmail.com>
2019-07-28 00:05:15 +0200

    pi model/scanning resolution to variables

--- bh1750-daemon.py
+++ bh1750-daemon.py
@@ -4,42 +4,23 @@ import time
 import paho.mqtt.client as mqtt
 from configparser import ConfigParser
 import json
-from collections import deque
 
 config = ConfigParser(delimiters=('=', ))
 config.read('config.ini')
 
-topic = config['mqtt'].get('topic', 'temperature/dht22')
+topic = config['mqtt'].get('topic', 'lux/luxtest')
 decim_digits = config['sensor'].getint('decimal_digits', 2)
 sleep_time = config['sensor'].getint('interval', 60)
+pitype = config['sensor'].get('pitype', 'smbus.SMBus(1)')
+luxres = config['sensor'].get('luxres', 0x20)
 
-
+bus = smbus.SMBus(int(pitype))
 
 # Define some constants from the datasheet
 DEVICE     = 0x23 # Default device I2C address
 POWER_DOWN = 0x00 # No active state
 POWER_ON   = 0x01 # Power on
 RESET      = 0x07 # Reset data register value
-# Start measurement at 4lx resolution. Time typically 16ms.
-CONTINUOUS_LOW_RES_MODE = 0x13
-# Start measurement at 1lx resolution. Time typically 120ms
-CONTINUOUS_HIGH_RES_MODE_1 = 0x10
-# Start measurement at 0.5lx resolution. Time typically 120ms
-CONTINUOUS_HIGH_RES_MODE_2 = 0x11
-# Start measurement at 1lx resolution. Time typically 120ms
-# Device is automatically set to Power Down after measurement.
-ONE_TIME_HIGH_RES_MODE_1 = 0x20
-# Start measurement at 0.5lx resolution. Time typically 120ms
-# Device is automatically set to Power Down after measurement.
-ONE_TIME_HIGH_RES_MODE_2 = 0x21
-# Start measurement at 1lx resolution. Time typically 120ms
-# Device is automatically set to Power Down after measurement.
-ONE_TIME_LOW_RES_MODE = 0x23
-
-#bus = smbus.SMBus(0) # Rev 1 Pi uses 0
-bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
-
-
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -56,20 +37,14 @@ client.connect(config['mqtt'].get('hostname', 'homeassistant'),
                config['mqtt'].getint('timeout', 60))
 client.loop_start()
 
-
-
-
 def convertToNumber(data):
   # Simple function to convert 2 bytes of data
   # into a decimal number
   return ((data[1] + (256 * data[0])) / 1.2)
 
-
 def readLight(addr=DEVICE):
-  data = bus.read_i2c_block_data(addr,ONE_TIME_HIGH_RES_MODE_2)
+  data = bus.read_i2c_block_data(addr,luxres)
   return convertToNumber(data)
-
-
 
 while True:
 
